081426b6e10b7b87197a3661dee684ace647ff92
Paulmahaut <paul.mahaut@gmail.com>
2024-02-20 16:28:59 +0100

    Création d'une feneêtre (Par Aube)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame as py
 from sys import exit
 
@@ -13,12 +15,16 @@ clock = py.time.Clock()
 screen = py.display.set_mode((600,600))
 py.display.set_caption('Game')
 
+# Background design
+background = py.Surface((600,600))
+background.fill(COLOR["gray"])
+
 while True:
     for event in py.event.get():
         if event.type == py.QUIT:
             py.quit()
             exit()
-        
     
+    screen.blit(background, (0,0))
     py.display.update()
     clock.tick(60)
