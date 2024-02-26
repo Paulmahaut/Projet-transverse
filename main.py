9dc985109d4aa66d3a38931e4214c879020d3575
Paulmahaut <paul.mahaut@gmail.com>
2024-02-26 20:28:08 +0100

    Ajout d'un screamer
    Non-fonctionnel

--- main.py
+++ main.py
@@ -13,26 +13,36 @@ py.init()
 clock = py.time.Clock()
 screen = py.display.set_mode((1600,1000))
 py.display.set_caption('Game')
-
+screamer=py.USEREVENT+1
+py.time.set_timer(screamer, 10000)
 # Background design
 #background = py.Surface((600,600))
 #background.fill(COLOR["gray"])
 
 #background unicorn
 im=py.image.load("Backgroundunicorn.png")
-
+test=py.image.load("screamer.jpg")
 player = Character()
-
-
+image_display_start = None
+special_image = py.transform.scale(test, (300, 300))
 #sound
 song = py.mixer.Sound("tqt.mp3")
 
 while True:
+    current_time = py.time.get_ticks()
     for event in py.event.get():
         if event.type == py.QUIT:
             py.quit()
             exit()
+        elif event.type==screamer:
+                        # Marquer le début de l'affichage de l'image
+            image_display_start = current_time
     
+    if image_display_start:
+        if current_time - image_display_start <= 1000:  # 100 ms = 1/10 de seconde
+            screen.blit(special_image, (900, 900))  
+        else:
+            image_display_start = None  # Réinitialiser pour le prochain affichage
     song.play()
     keys_pressed = py.key.get_pressed()
 
