6b75b29e64c739e69105152ca5e89970d0c349a8
Savratis56 <151128419+Savratis56@users.noreply.github.com>
2024-03-07 19:30:00 +0100

    enemy projectiles

--- Enemy.py
+++ Enemy.py
@@ -18,9 +18,30 @@ class Enemy(py.sprite.Sprite):
         if not py.sprite.spritecollide(self, group_player,False,  py.sprite.collide_mask):
             self.rect.x-=1
 
-#class Tank_projectil(py.sprite.sprite):
+import math
+import random
+import time
+
+class Tank_project: #projectiles du Tank 
+    def __init__(self):
+        self.angle = 0
+        self.force = 0
+        Tank_project_image= py.image.load("Tank_proje.png")
+
+    def throw_projectile(self): #lancer les projectiles avec un angle variable choisit aleatoirement
+        self.angle = random.uniform(0, 90)  
+        self.force = random.uniform(10, 50) 
+          #je sais pas trop ce que ca fait je vais verifier apres mais c'est cense etre la trajectoire
+        time_of_flight = (2 * self.force * math.sin(math.radians(self.angle))) / 9.8
+        horizontal_distance = self.force * math.cos(math.radians(self.angle)) * time_of_flight
+        
     
- #   def 
+Tank= Tank_project()
+
+# pour que ca continue l'action automatiquement
+while True:
+    Tank.throw_projectile()
+    time.sleep(random.randint(0,10)) #pour qu'il y'ait un delai entre les lancers de projectiles
 
 
 
