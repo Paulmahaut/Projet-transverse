e89620366126088ccbb152e883ff13548036ff0b
BrunRaphael <151138375+BrunRaphael@users.noreply.github.com>
2024-03-17 14:00:01 +0100

    Implémentation de la barre de vie
    Ajout des fonctions de bases de la barre de vie du personnage ( affichage, fonction pour rajouter/enlever des point de vie, etc)

--- Licorne.py
+++ Licorne.py
@@ -13,6 +13,31 @@ class Character(py.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 100  # Position initiale x
         self.rect.y = 430  # Position initiale y
+        
+        self.current_health = 500 # Valeur initial de la barre de vie
+        self.maximum_health = 1000 # Valeur maximum de la barre de vie
+        self.health_bar_lenght = 400 # Longeur maximal en pixel de la barre de vie
+        self.health_ratio = self.maximum_health / self.health_bar_lenght # Ratio utiliser pour remplir la barre de vie
+    
+    def get_damage(self,amount):
+        if self.current_health > 0:
+            self.current_health -= amount # Baisse la valeur de la barre de vie de X 
+        if self.current_health <= 0:
+            self.current_health =0 # Eviter que la valeur de la barre de vie soit inférieur à 0
+    
+    def get_health(self,amount):
+        if self.current_health < self.maximum_health:
+            self.current_health += amount # Augmente la valeur de la barre de vie de X
+        if self.current_health >= self.maximum_health:
+            self.current_health = self.maximum_health # Evite que la valeur de labarre de vie dépasse le maximum
+    
+    def character_health(self):
+        pygame.draw.rect(screen, (255,0,0), (10,10,self.current_health/self.health_ratio,25)) # Rectangle de la barre de vie
+        pygame.draw.rect(screen, (255,255,255), (10,10,self.health_bar_lenght,25),4) # Bordure de la barre de vie
+
+    def update(self):
+        self.basic_health()
+        
 
         self.group_projectil = py.sprite.Group()
     
